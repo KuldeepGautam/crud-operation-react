f8384d3bbb31ee7f44516cc39ce26b58ba2bdcd8
kuldeep.gautam <kuldeep.gautam@sisrtd.com>
2022-10-14 17:59:27 +0530

    apiUrl change

--- src/components/ListUser.js
+++ src/components/ListUser.js
@@ -1,56 +1,61 @@
-import axios from "axios"
+import axios from "axios";
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
 export default function ListUser() {
+  const [users, setUsers] = useState([]);
+  useEffect(() => {
+    getUsers();
+  }, []);
 
-    const [users, setUsers] = useState([]);
-    useEffect(() => {
+  function getUsers() {
+    axios.get("http://192.168.0.182/api/customers").then(function (response) {
+      console.log(response.data);
+      setUsers(response.data);
+    });
+  }
+
+  const deleteUser = (id) => {
+    axios
+      .delete(`http://localhost:8888/api/user/${id}/delete`)
+      .then(function (response) {
+        console.log(response.data);
         getUsers();
-    }, []);
-
-    function getUsers() {
-        axios.get('http://localhost:8888/api/users/').then(function(response) {
-            console.log(response.data);
-            setUsers(response.data);
-        });
-    }
-
-    const deleteUser = (id) => {
-        axios.delete(`http://localhost:8888/api/user/${id}/delete`).then(function(response){
-            console.log(response.data);
-            getUsers();
-        });
-    }
-    return (
-        <div>
-            <h1>List Users</h1>
-            <table>
-                <thead>
-                    <tr>
-                        <th>#</th>
-                        <th>Name</th>
-                        <th>Email</th>
-                        <th>Mobile</th>
-                        <th>Actions</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {users.map((user, key) =>
-                        <tr key={key}>
-                            <td>{user.id}</td>
-                            <td>{user.name}</td>
-                            <td>{user.email}</td>
-                            <td>{user.mobile}</td>
-                            <td>
-                                <Link to={`user/${user.id}/edit`} style={{marginRight: "10px"}}>Edit</Link>
-                                <button onClick={() => deleteUser(user.id)}>Delete</button>
-                            </td>
-                        </tr>
-                    )}
-                    
-                </tbody>
-            </table>
-        </div>
-    )
+      });
+  };
+  return (
+    <div>
+      <h1>List Users</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Mobile</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          {users.map((user, key) => (
+            <ul key={key}>
+              <li>{user.response.data && user.response.data.name}</li>
+              {/* <td>{user.name}</td>
+              <td>{user.email}</td>
+              <td>{user.mobile}</td>
+              <td>
+                <Link
+                  to={`user/${user.id}/edit`}
+                  style={{ marginRight: "10px" }}
+                >
+                  Edit
+                </Link>
+                <button onClick={() => deleteUser(user.id)}>Delete</button>
+              </td> */}
+            </ul>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
 }
