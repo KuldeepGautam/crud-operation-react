bd6b5fd2b5148bb82a0afeb5ef78d5309640d6aa
kuldeep.gautam <kuldeep.gautam@sisrtd.com>
2022-11-30 15:19:05 +0530

    Change user name

--- src/components/ListUser.js
+++ src/components/ListUser.js
@@ -9,8 +9,7 @@ export default function ListUser() {
   }, []);
 
   function getUsers() {
-    axios.get("http://192.168.0.182/api/customers").then(function (response) {
-      // axios.get("http://192.168.0.182/api/customers").then(function (response) {
+    axios.get("http://192.168.0.158/api/customers").then(function (response) {
       console.log(response.data);
       setState(response.data.response.data);
     });
